6b21013a9595262ff545f1e3ca1e947636405be8
Vlad Gritsavka <griczavka2363@gmail.com>
2023-01-25 18:02:09 +0300

    BrowserRouter was changed to HashRouter

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,16 +1,16 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from 'components/Home/Home';
 import UserModal from 'components/user/UserModal/UserModal';
 import ProtectedRoute from 'components/shared/ProtectedRoute/ProtectedRoute';
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path='/' element={<ProtectedRoute outlet={<Home />} />}></Route>
 				<Route path='sign-in' element={<UserModal />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
